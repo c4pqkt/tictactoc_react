1eb27c083faf0943acd831979522ab6b5b97ae14
c4pqkt <c4pqkt@yahoo.com>
2024-09-03 09:27:50 -0500

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,47 @@
-import logo from './logo.svg';
-import './App.css';
+// Este módulo mantiene el uso de estados dentro del componente
 
-function App() {
+// function Square({ value }) {
+//   return <button className="square">{value}</button>;
+// }
+
+import { useState } from 'react';
+
+
+function Square() {
+  const [value, setValue] = useState(null);
+
+  function handleClick() {
+    setValue('X');
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <button
+      className="square"
+      onClick={handleClick}
+    >
+      {value}
+    </button>
   );
 }
 
-export default App;
+export default function Board() {
+  return (
+     <>
+      <div className="board-row">
+        <Square />
+        <Square />
+        <Square />
+      </div>
+      <div className="board-row">
+        <Square />
+        <Square />
+        <Square />
+      </div>
+      <div className="board-row">
+        <Square />
+        <Square />
+        <Square />
+      </div>
+    </>
+  );
+}
--- src/index.js
+++ src/index.js
@@ -1,12 +1,23 @@
+// React JS tutorial
+// Code from 
+// https://react.dev/learn/tutorial-tic-tac-toe
+// revision 21 de agosto de 2024 
+
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+import App2 from './App2';
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
+    <App2 />
+    <br />
+    <br />
+    <hr />
+    <h1>Next module manages states inside the component</h1>
     <App />
   </React.StrictMode>
 );
